088871a37b4d7f30899e95ef29ae2ab3604d79ff
elbekkaye taha <elbekkayetaha26@gmail.com>
2024-01-04 01:01:24 +0100

    ajouter et appeler multiple

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,6 +3,8 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+	}
+	public static void main(String[] args) {
 		System.out.println(Calculatrice.multiple(3, 3));
 	}
 }
