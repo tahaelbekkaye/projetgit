3b55c41fe08ced9eca2979d8b3e7329856fa3495
najat agou <najatagou2030@gmail.com>
2024-01-05 02:42:09 +0100

    Correction du bug

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,7 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		int resultatAddition = Calculatrice.add(5, 3);
+		System.out.println("Résultat de l'addition : " + resultatAddition);
 	}
 }
